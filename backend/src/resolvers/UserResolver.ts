93e9bcccb9c676ec462f7174926471c8889eb5b3
Adalberto Junior <adljunior@rd.com.br>
2022-06-08 16:04:31 -0300

    back and front

---
+++ backend/src/resolvers/UserResolver.ts
@@ -0,0 +1,28 @@
+import { Arg, Mutation, Query, Resolver } from "type-graphql";
+import { User } from "./../models/User";
+import crypto from 'crypto'
+
+// query: Buscar dados
+// mutation: Criar, alterar ou deletar
+
+@Resolver()
+export class UserResolver {
+  private data: User[] = [];
+
+  @Query(() => [User])
+  async users() {
+    return this.data;
+  }
+
+  @Mutation(() => User)
+  async createUser(
+    @Arg('name') name: string
+  ) {
+    const user = {id: crypto.randomUUID(), name}
+
+    this.data.push(user)
+
+    return user;
+  }
+
+}
